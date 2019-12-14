5f27b88a841be99f978945006abfeddca45fee5c
dr6am <alexanderbasoc@gmail.com>
2019-12-14 14:11:03 +0300

    Updates

---
+++ precache-manifest.0f720d75361fadf6927ffc0bc633f900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f25af3e9f0b66a84b568cc8e69e7e4",
+    "url": "/plant-store/index.html"
+  },
+  {
+    "revision": "300f30d40a9491e85422",
+    "url": "/plant-store/static/css/main.abf5a090.chunk.css"
+  },
+  {
+    "revision": "a1ff3b18996d0f53ddef",
+    "url": "/plant-store/static/js/2.b4505408.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/plant-store/static/js/2.b4505408.chunk.js.LICENSE"
+  },
+  {
+    "revision": "300f30d40a9491e85422",
+    "url": "/plant-store/static/js/main.05664916.chunk.js"
+  },
+  {
+    "revision": "8a51d01c5d512513b35a",
+    "url": "/plant-store/static/js/runtime-main.aefa9ddd.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/plant-store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
