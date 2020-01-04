d77f7783c431b031ff54b0a1c65f6c4109bfed6e
dr6am <alexanderbasoc@gmail.com>
2020-01-04 15:37:45 +0300

    replaced redux to context api and optimized for mobile

---
+++ build/precache-manifest.bb0c0b80fdc4970f5f65f1a85470330a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c366d47c9fefef0a95dfe4a1c92622a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce2fd33da7659bee646",
+    "url": "/static/css/main.c1364220.chunk.css"
+  },
+  {
+    "revision": "402b4cf79937c1f8b8f2",
+    "url": "/static/js/2.c1b0cefd.chunk.js"
+  },
+  {
+    "revision": "bece73c9988dcb614abc759d33b51b13",
+    "url": "/static/js/2.c1b0cefd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dce2fd33da7659bee646",
+    "url": "/static/js/main.a380876b.chunk.js"
+  },
+  {
+    "revision": "78caae2cb1504e2594c9",
+    "url": "/static/js/runtime-main.4c34bd5e.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== "production") require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE_ENV === "production") {
+
+}
+
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
+app.post('/payment', (req, res) => {
+	const body = {
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: 'usd'
+	};
+	stripe.charges.create(body,(e, StripeRes)=>{
+		
+		if(e){
+			res.status(500).send({error:e})
+		}
+		else{
+			res.status(200).send({success:StripeRes})
+		}
+		
+	}
+	)
+})
+
+
+app.listen(port, () => {
+	console.log("\t App listening on port " + port + "\t", Date(Date.now()).toString())
+})
